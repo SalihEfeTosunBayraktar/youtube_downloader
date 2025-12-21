a79942679839642c24419d1239639fe57386e055
Legendnoobe <trozge73@gmail.com>
2025-12-21 17:11:50 +0300

    build: Update PyInstaller build version from v1.7 to v1.8.

--- build_exe.bat
+++ build_exe.bat
@@ -6,11 +6,11 @@ if %errorlevel% neq 0 (
     python -m pip install -r requirements.txt
 )
 
-echo Building EXE v1.7...
-py -m PyInstaller --noconsole --onefile --name "YoutubeDownloader_v1.7" --icon=app_icon.ico --add-data "themes;themes" --add-data "app_icon.ico;." --add-binary "ffmpeg.exe;." --distpath "%USERPROFILE%\Downloads" main.py
+echo Building EXE v1.8...
+py -m PyInstaller --noconsole --onefile --name "YoutubeDownloader_v1.8" --icon=app_icon.ico --add-data "themes;themes" --add-data "app_icon.ico;." --add-binary "ffmpeg.exe;." --distpath "%USERPROFILE%\Downloads" main.py
 if %errorlevel% neq 0 (
     echo "py command failed, trying python..."
-    python -m PyInstaller --noconsole --onefile --name "YoutubeDownloader_v1.7" --icon=app_icon.ico --add-data "themes;themes" --add-data "app_icon.ico;." --add-binary "ffmpeg.exe;." --distpath "%USERPROFILE%\Downloads" main.py
+    python -m PyInstaller --noconsole --onefile --name "YoutubeDownloader_v1.8" --icon=app_icon.ico --add-data "themes;themes" --add-data "app_icon.ico;." --add-binary "ffmpeg.exe;." --distpath "%USERPROFILE%\Downloads" main.py
 )
 
 echo Build Complete! EXE saved to Downloads folder.
